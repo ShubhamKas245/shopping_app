09c4edb0f89093587495986f8d7ef1d959926d5e
ShubhamKas245 <ShubhamKas245@gmail.com>
2023-05-28 20:37:13 +0530

    Basic Setup Completed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-2xl font-bold my-8 px-4">Welcome to Online-Shopping App</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
